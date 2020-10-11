67e21871aadc36d375ff5f1524dcf3245c898d77
oycqwty <55989150@qq.com>
2020-10-11 14:45:47 +0800

    init springboot project

---
+++ src/main/java/com/kevin/boot/KevinBootApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KevinBootApplication.class, args);
+    }
+
+}
